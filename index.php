7e72ebe74e7730c9fc65a2687c022bc26f6a74ca
LaurynasG <glaurynasg@gmail.com>
2020-10-13 09:39:00 +0300

    first task

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-print 'Veikia';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aš valdau HTML!</title>
+</head>
+<body>
+    <h1>Laurynas - HTML asas!</h1>
+    <p>Moku kurti statinį turinį.</p>
+</body>
+</html>
