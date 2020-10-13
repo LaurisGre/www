8268c0b39bc9b4b8677f28cfb2e7bb804e30837f
LaurynasG <glaurynasg@gmail.com>
2020-10-13 11:10:26 +0300

    second task

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
+<?php date_default_timezone_set('Europe/Vilnius'); ?>
 <!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aš valdau HTML!</title>
+    <title><?php print date('D') . ' ir PHP'; ?></title>
 </head>
+
 <body>
     <h1>
         <i>Laurynas</i>
-        <span style="font-weight:normal"> - HTML asas!</span>
+        <span style='font-weight:normal'>
+            - HTML<?php print ' ir PHP' ?> asas jau nuo <?php print date('Y') . ' metų' ?>
+        </span>
     </h1>
-    <p>Moku kurti statinį turinį.</p>
+    <p>Viskas prasidėjo <?php print date('M') . ' mėnesio, ' . date('D') . ' dieną!'; ?></p>
 </body>
+
 </html>
