c7aeee3543a4e4a14fdefbaccc00ba99a1da76c8
LaurynasG <glaurynasg@gmail.com>
2020-10-21 10:42:50 +0300

    car_value

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-    $speed_of_sound = 333;
-    $max_db = 120;
+    $months = 24;
+    $car_price_new = 30000;
+    $depreciation = 2;
+    $car_price_used2 = 30000;
+
+    for ($i = 0; $i <= 24; $i++) {
+        $car_price_used = $car_price_new * ((100 - $depreciation) / 100) ** $i;
+        // $car_price_used2 *= ((100 - $depreciation) / 100);
+    }
+    $car_price_used_f = number_format($car_price_used, 2);
+    $depr_perc = number_format(100 - $car_price_used * 100 / $car_price_new, 2);
+    $h2 = "Naujos mašinos kaina: $car_price_new eur";
+    $h3 = "Po $months mėn, mašinos vertė bus: $car_price_used_f eur";
+    // $h33 = "Po $months mėn, mašinos vertė bus: $car_price_used2 eur";
+    $h4 = "Mašina nuvertės $depr_perc proc.";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,15 +24,10 @@
     </style>
 </head>
 <body>
-    <h1>Griaustinio zona</h1>
-    <?php for($t = 0; $t <= 60; $t++): ?>
-        <p>
-            <?php 
-                $dist = $t * $speed_of_sound;
-                $sound = round(($max_db * 0.91 ** ($dist/100)), 3);
-                print "Po $t sec $dist m. : $sound db"; 
-            ?>
-        </p> 
-    <?php endfor; ?>
+    <h1>Kiek nuvertės mašina?</h1>
+    <h2><?php print $h2; ?></h2>
+    <h3><?php print $h3; ?></h3>
+    <!-- <h3><?php print $h33; ?></h2> -->
+    <h4><?php print $h4; ?></h4>
 </body>
 </html>
