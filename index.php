07945d303a5f6f1f9ce4903f30e959fb249faee4
LaurisDesktop <glaurynasg@gmail.com>
2020-10-29 13:10:22 +0200

    print array

--- index.php
+++ index.php
@@ -1,30 +1,41 @@
 <?php
 
-function generate_matrix($size) {
-    $new_matrix = [];
-    for($i = 0 ; $i < $size ; $i++) {
-        $new_row = [];
-        for($u = 0 ; $u < $size ; $u++) {
-            $new_row[] = rand(0, 1);
-        }
-        $new_matrix[] = $new_row;
-    }
-    return $new_matrix;
-}
-
-$matrix = generate_matrix(8);
-
-function get_winning_rows($mat) {
-    $winning_rows = [];
-    foreach($mat as $key => $row) {
-        if(count(array_unique($row)) === 1){
-            $winning_rows[] = $key;
+function print_array($data) {
+    $string = '';
+    if (gettype($data) != 'array') {
+        $string .= $data . '.';
+    } else {
+        foreach($data as $value) {
+            $string .= print_array($value);
+            // var_dump($string);
         }
     }
-    return $winning_rows;
+    var_dump($string);
+    if($string[-1] === '.') {
+        $string .= ',';
+    }
+    return $string;
 }
 
-$winners = get_winning_rows($matrix);
+$arr5 = [
+    'stringas',
+    'vardas' => [
+        'stringas',
+        'dar viena verte' => [
+            1,
+            2,
+            4,
+            'random' => [
+                'veikia?'
+            ]
+        ]
+    ],
+    1000
+];
+
+$test_array = print_array($arr5);
+var_dump($test_array);
+
 
 ?>
 <!DOCTYPE html>
@@ -35,20 +46,6 @@ $winners = get_winning_rows($matrix);
     <title>Motrix</title>
 </head>
 <body>
-    <article class='matrix_box'>
-        <?php foreach($matrix as $key => $row): ?>
-            <section class="row <?php if (in_array($key, $winners)) print 'winner'; ?>">   
-                <?php foreach($row as $square): ?>
-                    <div class='square <?php print $square ? 'gold' : 'blue'; ?>'></div>
-                <?php endforeach; ?>
-            </section>
-        <?php endforeach; ?>
-    </article>
-    <article>
-        <section>Winning rows:</section>
-        <?php foreach($winners as $row): ?>
-            <p><?php print ($row +1 ); ?></p>
-        <?php endforeach; ?>
-    </article>
+
 </body>
 </html>
