391cb082d67d4b94df5213fc8d93ec58106d8eef
LaurisDesktop <glaurynasg@gmail.com>
2020-11-03 10:19:45 +0200

    simple form

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <?php
 
-$atsakymas = '';
+$title = 'Užpildyk mane:';
 
-if (isset($_POST['number'])) {
-    switch ($_POST['button']) {
-        case 'square':
-            $atsakymas = $_POST['number'] ** 2;
-            break;
-        case 'cube':
-            $atsakymas = $_POST['number'] ** 3;
-            break;
-        case 'root':
-            $atsakymas = sqrt($_POST['number']);
-            break;
+$atsakymas = [];
+$result = '';
+$form_vis = '';
+$cert_vis = '';
+
+if (isset($_POST['button'])) {
+    $title = 'PHP ANKETA';
+    $form_vis = 'form_hide';
+    $cert_vis = 'cert_show';
+
+    foreach($_POST as $key => $input) {
+        if($key != 'button') {
+            if($key === 'Amžius') {
+                $atsakymas[$key] = 'Amžius' . (2020 - $input);
+            } else {
+                $atsakymas[$key] = "$key: $input";
+            }
+        }
     }
 }
+
+$result = $_POST['Vardas'] . ', gimęs(-usi) ' . $_POST['Amžius'] . ' metais, yra ' . $_POST['Lygis'] . ' PHP programuotojas';
 
 ?>
 <!DOCTYPE html>
@@ -25,12 +34,23 @@ if (isset($_POST['number'])) {
     <title>Form</title>
 </head>
 <body>
-    <h1><?php print $atsakymas; ?></h1>
-    <form method="POST">
-        <input type="number" name='number'>
-        <input type="submit" name="button" value='square'>
-        <input type="submit" name='button' value='cube'>
-        <input type="submit" name="button" value='root'>
+    <h1><?php print $title; ?></h1>
+    <div class="certificate <?php print $cert_vis; ?>"></div>
+    <form method="POST" class="<?php print $form_vis; ?>">
+        <input type="text" name='Vardas' placeholder="name">
+        <input type="text" name='Pavardė' placeholder="surname">
+        <input type="number" name="Amžius" placeholder="year">
+        <p>Kaip vertini savo PHP žinias?</p>
+        <select name="Lygis">
+            <option value="durnas">durnas</option>
+            <option value="labai durnas">labai durnas</option>
+            <option value="neblogas">neblogas</option>
+        </select>
+        <input type="submit" name='button' value='submit'>
     </form>
+    <?php foreach($atsakymas as $key => $value): ?>
+        <p><?php print $value; ?></p>
+    <?php endforeach; ?>
+    <p><?php print $result; ?></p>
 </body>
 </html>
